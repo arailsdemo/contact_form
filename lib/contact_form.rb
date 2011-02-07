4327184f53fc89d537c2fc3011e4e04f8aedf245
aRailsDemo <arailsdemo@gmail.com>
2011-02-07 08:43:24 -0800

    Rails Engines

--- lib/contact_form.rb
+++ lib/contact_form.rb
@@ -1,2 +1,4 @@
 module ContactForm
+  require 'contact_form/engine'
+  autoload :Form, 'contact_form/form'
 end
---
+++ lib/contact_form/engine.rb
@@ -0,0 +1,8 @@
+module ContactForm
+  class Engine < Rails::Engine
+#    paths.app << 'lib/app'
+#    paths.app.views << 'lib/app/views'
+#    paths.config.routes << "lib/config/routes.rb"
+#    paths.config.locales << 'lib/config/locales'
+  end
+end
